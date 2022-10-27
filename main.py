5683837776db9e025309ed533dba3e31a3071440
Arturo Martinez <113930370+amartinez500@users.noreply.github.com>
2022-10-27 17:53:31 +0000

    ReturnStuff Practice 1,2,3

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #from methods_help import method_help
-from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome, square
-#from returnStuff import returnS
+#from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome, square
+from returnStuff import power
 
 #method_help()
 #function()
-yari = welcome(" Yahaira")
-added = add_numbers(4,5)
-print(added)
+#yari = welcome(" Yahaira")
+#added = add_numbers(4,5)
+#print(added)
 
-final = num_dividedFrom_add_numbers(45, added)
-print(final)
-
-greet()
-
-art = welcome(" Arturo")
-print(art)
+#final = num_dividedFrom_add_numbers(45, added)
+#print(final)
+#returnS()
+#greet()
+power2 = power(3,7)
+print(power2)
+#art = welcome(" Arturo")
+#print(art)
 
 #returnS()
